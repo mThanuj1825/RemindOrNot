c5be6888cbc7dee17db8b08e6f447fb4bf6259de
M Thanuj <thanujmullaguri1825@gmail.com>
2024-11-10 09:48:01 +0530

    Backend URI

--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:3333",
+  baseURL: "https://remindornot-backend.onrender.com/",
 });
 
 axiosInstance.interceptors.request.use((config) => {
